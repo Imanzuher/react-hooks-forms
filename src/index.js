1f837e46f82019f108a3d7fe0874f641d1a656ae
Imanzuher <imoojaf@gmail.com>
2022-10-05 00:20:49 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Form from './components/Form'
-
-ReactDOM.render(
-  <Form />,
-  document.getElementById('root')
-);
+import React from "react";
+import ReactDOM from "react-dom";
+import ParentComponent from "./components/ParentComponent";
+ReactDOM.render(<ParentComponent />, document.getElementById("root"));
